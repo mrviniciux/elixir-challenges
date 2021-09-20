8d10bb14d1c8b0b0c137e8e376df3ca35b005263
Marcos Santos <marcos@intellimize.com>
2021-09-20 17:32:03 -0300

    New challenge: secrets

--- lib/Secrets/secrets.ex
+++ lib/Secrets/secrets.ex
@@ -1,4 +1,5 @@
 defmodule Secrets do
+    use Bitwise
     @moduledoc ~S"""
     Anon function exercise. 
     
@@ -17,4 +18,29 @@ defmodule Secrets do
       #&(&1 + secret)
       fn to_add -> to_add + secret end
    end
+
+
+  def secret_subtract(secret) do
+    &(&1 - secret)
+  end
+
+  def secret_multiply(secret) do
+   &(trunc(&1 * secret))
+  end
+
+  def secret_divide(secret) do
+   &(trunc(&1 / secret))
+  end
+
+  def secret_and(secret) do
+    &(Bitwise.&&&(secret, &1))
+  end
+
+  def secret_xor(secret) do
+    &(Bitwise.^^^(secret, &1))
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    &(secret_function1.(&1) |> secret_function2.())
+  end
 end
---
+++ test/secret_test.exs
@@ -0,0 +1,15 @@
+defmodule SecretTest do
+    use ExUnit.Case
+    doctest Secrets
+  
+    test "test secret_combine 32 divide by 3 then multiply 5" do
+      multiply = Secrets.secret_divide(3)
+      divide = Secrets.secret_add(5)
+      combined = Secrets.secret_combine(multiply, divide)
+
+      assert combined.(32) == 15
+    end
+
+
+  end
+  
